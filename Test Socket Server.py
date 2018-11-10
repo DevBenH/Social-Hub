f102305ee80a70ded59cba0cb9802b65bf037983
Monty Burrows <40180108+MontyBurrows@users.noreply.github.com>
2018-11-10 17:42:38 +0000

    Update Test Socket Server.py

--- Test Socket Server.py
+++ Test Socket Server.py
@@ -9,22 +9,22 @@
 # Licence:     N/A                                                      #
 #-----------------------------------------------------------------------#
 
-import socket
-from threading import *
+import socket                                                           #Imports socket
+from threading import *                                                 #Imports all from threading
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "localhost"
-port = 194
-print(host+":"+str(port))
-s.bind((host, port))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                   #SOCK_STREAM opens the TCP socket; AF_INET specifies to use IPv4
+host = "localhost"                                                      #Sets IPv4 of server - localhost -> yourself
+port = 194                                                              #Sets port to use
+print(host+":"+str(port))                                               #For testing purposes, to see if the details to connect are correct
+s.bind((host, port))                                                    #Binds values to server
 
 class client(Thread):
     def __init__(self, socket, address):
         Thread.__init__(self)
-        self.sock = socket
-        self.addr = address
-        self.start()
-
+        self.sock = socket                                              #Defining self.sock to socket(s)
+        self.addr = address                                             #Defining self.address to address of client
+        self.start()                                                    #Start
+        
     def run(self):
         while 1:
             global ret
